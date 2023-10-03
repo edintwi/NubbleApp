18f48cfae66e8254caf15247f84733ce1dc49c00
Edson Brandon <edsonbrandon8@gmail.com>
2023-10-03 17:22:03 -0300

    feat: adding font family in text component

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ function App(): JSX.Element {
 
   return (
     <SafeAreaView>
-      <Text preset='headingLarge' style={{color: 'red', fontFamily: 'Satoshi-Black'}}>
+      <Text bold italic preset='headingLarge' style={{color: 'red'}}>
         CoffStack
       </Text>
     </SafeAreaView>
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -7,19 +7,50 @@ import {
 
 interface TextProps extends RNTextProps {
   preset?: TextVariants;
+  bold?: boolean;
+  semibold?: boolean;
+  italic?: boolean;
 }
 
 export function Text({
   children,
   preset = 'paragraphMedium',
   style,
+  bold,
+  italic,
+  semibold,
   ...rest
 }: TextProps) {
+
+  const fontFamily = getFontFamily(preset, bold, italic, semibold);
+
   return (
-    <RNText style={[$fontSizes[preset], style]} {...rest}>
+    <RNText style={[$fontSizes[preset], {fontFamily}, style]} {...rest}>
       {children}
     </RNText>
   );
+}
+
+function getFontFamily(preset: TextVariants, bold?: boolean, semibold?: boolean, italic?: boolean) {
+  
+  if(preset === 'headingLarge' || preset === `headingMedium` || preset === `headingSmall`){
+    return italic ?  $fontFamily.boldItalic : $fontFamily.bold;
+  }
+  
+  switch (true) {
+    case bold && italic:
+      return $fontFamily.boldItalic;
+    case bold:
+      return $fontFamily.bold;
+    case italic:
+      return $fontFamily.italic;
+    case semibold && italic:
+      return $fontFamily.mediumItalic;
+    case semibold:
+      return $fontFamily.medium;
+    default:
+      return $fontFamily.regular;
+  }
 }
 
 type TextVariants =
@@ -43,4 +74,17 @@ const $fontSizes: Record<TextVariants, TextStyle> = {
 
   paragraphCaption: {fontSize: 12, lineHeight: 16.8},
   paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
+
+const $fontFamily = {
+  black: 'Satoshi-Black',
+  blackItalic: 'Satoshi-BlackItalic',
+  bold: 'Satoshi-Bold',
+  boldItalic: 'Satoshi-BoldItalic',
+  italic: 'Satoshi-Italic',
+  light: 'Satoshi-Light',
+  lightItalic: 'Satoshi-LightItalic',
+  medium: 'Satoshi-Medium',
+  mediumItalic: 'Satoshi-MediumItalic',
+  regular: 'Satoshi-Regular',
 };
