2f4b6a707101ca481c699ed2348627292a52248d
Edson Brandon <edsonbrandon8@gmail.com>
2023-11-30 17:10:48 -0300

    feat: applying adapter desing pattern

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,9 +1,20 @@
-import {postListMock} from './postListMock';
-import {Post} from './types';
+import {PageAPI} from '@api';
 
-async function getList(): Promise<Post[]> {
-  await new Promise(resolve => setTimeout(() => resolve(''), 1000));
-  return postListMock;
+import {PostAPI} from './postTypes';
+
+async function getList(): Promise<PageAPI<PostAPI>> {
+  let response = await fetch('http://10.0.2.2:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer NQ.Ubst0pjK0n2KR6UD8BKWDLTnMP9pV2-FsASiAEAqXipya727wvwwFMlLf4jy',
+    },
+  });
+
+  let data: PageAPI<PostAPI> = await response.json();
+  console.log(data);
+
+  return data;
 }
 
 export const postApi = {
