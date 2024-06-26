5f2a2e06698ba2a3d16eebafcd916b4980070125
Edson Brandon <edsonbrandon8@gmail.com>
2024-06-26 10:43:49 -0300

    feat: auth credentials local storage

--- src/services/authCredentials/useAuthCredentials.ts
+++ src/services/authCredentials/useAuthCredentials.ts
@@ -1,14 +1,24 @@
 import {create} from 'zustand';
 
+import {persist} from 'zustand/middleware';
+import {storage} from '../storage/storage';
 import {AuthCredentialsService} from './authCredentialsTypes';
 
 export function useAuthCredentials(): AuthCredentialsService {
   return useAuthCredentialsZustand();
 }
 
-const useAuthCredentialsZustand = create<AuthCredentialsService>(set => ({
-  authCredentials: null,
-  isLoading: false,
-  saveCredentials: async ac => set({authCredentials: ac}),
-  remove: async () => set({authCredentials: null}),
-}));
+const useAuthCredentialsZustand = create<AuthCredentialsService>()(
+  persist(
+    set => ({
+      authCredentials: null,
+      isLoading: false,
+      saveCredentials: async ac => set({authCredentials: ac}),
+      removeCredentials: async () => set({authCredentials: null}),
+    }),
+    {
+      name: '@Auth',
+      storage: storage,
+    },
+  ),
+);
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export * from './authCredentials/index';
+export * from './storage/storage';
 export {ToastProvider} from './toast/Providers/ToastProvider';
 export * from './toast/toastTypes';
 export * from './toast/useToast';
